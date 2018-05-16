922e98cbb6a4985ccc88f02ef453b2820a490258
Matheus Teixeira Lemos <37637115+MatheusTLemos@users.noreply.github.com>
2018-05-16 00:26:57 -0300

    Delete UserAuthentication.php

--- TP2Web/UserAuthentication.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $mysql = 'mysql:host=localhost;dbname=analyses';
-  $dbusername = 'root';
-  $dbpassword = '';
-  $db = new PDO($mysql, $dbusername, $dbpassword);
-
-  $procedures = $db->query("SELECT email, password FROM users WHERE email='" . $email . "'");
-
-?>
